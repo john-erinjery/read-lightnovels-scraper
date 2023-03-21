110e6c760fce5eee5929a64ea79eaf1fb2c3cda7
john-erinjery <jancyvinod415@gmail.com>
2023-03-21 21:08:31 +0530

    soup commit

--- extractor.py
+++ extractor.py
@@ -3,6 +3,7 @@ import bs4
 from pickle import dump
 from fpdf import FPDF
 import requests
+import codecs
 import os
 soup = bs4.BeautifulSoup(open('body.html', mode='r', encoding='utf-8'), 'xml')
 pdf = FPDF()
@@ -12,7 +13,6 @@ pdf.set_font("Times", size = 13)
 def extract_links_from_body() -> dict:
     soup = bs4.BeautifulSoup(open('body.html', mode='r', encoding='utf-8'), 'xml')
     global n
-    n = 1
     link_dict = {}
     for i in soup.find_all('a'):
         if i.parent.parent.name == 'ul':
@@ -21,8 +21,12 @@ def extract_links_from_body() -> dict:
                     if j.name == 'span':
                         if j.has_attr('class'):
                             if j['class'] == 'chapter-text':
-                                link_dict[n] = i['href']
-                                n += 1
+                                text = j.text
+                                n = ''
+                                for k in text.split()[1]:
+                                    if k.isdigit():
+                                        n += k
+                                link_dict[int(n)] = i['href']
     return link_dict
 
 def create_links_dat():
@@ -60,27 +64,33 @@ def split_at_words_(line, num):
     return line_list
 
 def download_chapter_with_url(_url):
-    r = requests.get(url=_url)
+    r = requests.get(url=_url, stream=True)
     r.encoding = 'UTF-8'
-    return r.content.decode().replace('&#8217;', "'").replace('&#8230;', '...').replace('&#8220;', '"').replace('&#8221;', '"')
+    soup = bs4.BeautifulSoup(r.content, 'xml')
+    decoded_data = codecs.decode(soup.prettify('UTF-8'), 'UTF-8').replace('‘', "'").replace('’', "'").replace('“', '"').replace('”', '"').replace('…', '...').replace('–', '-').replace('―', '-').replace('　', ' ')
+    soup_2 = bs4.BeautifulSoup(decoded_data, 'xml')
+    return soup_2
+    
 
 def get_chapter_content(markup):
-    soup = bs4.BeautifulSoup(markup, 'xml')
-    div = soup.find(name='div', attrs={'class' : 'chapter-content'})
-    content = []
-    for i in div.children:
-        if i.name == 'p':
-            content.append(str(i.get_text()+ '\n'))
+    div = markup.find(name='div', attrs={'class' : 'chapter-content'})
     if os.path.exists('content.txt'):
         pass
     else:
         open('content.txt', 'w')
     with open('content.txt', 'a') as f:
-            f.writelines(content)
+        for i in div.children:
+            if i.name == 'p':
+                try:
+                    f.write(str(i.get_text().strip()+ '\n'))
+                except:
+                    print('character error, ripping off one line. : ')
+                    print(i.get_text(), '\n')
 
-def text_to_pdf_converter(file):
+
+def text_to_pdf_converter(file, range_):
     for i in file:
         lines = split_at_words_(i, 100)
         for j in lines:
             pdf.cell(300, 7, txt=j, ln = 1, align = 'L')
-    pdf.output("output.pdf")
+    pdf.output(f"{range_[0]}-{range_[1]}.pdf")
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ for i in range(range_[0], range_[1] + 1):
     print('\ndownloading and processing chapter..', i)
     extractor.get_chapter_content(extractor.download_chapter_with_url(link_dict[i]))
 print('\nconverting to PDF..')
-extractor.text_to_pdf_converter(open('content.txt', 'r', encoding='utf-8', errors='ignore'))
+extractor.text_to_pdf_converter(open('content.txt', 'r', encoding='utf-8', errors='ignore'), range_)
 print('\ndeleting content.txt and cache..')
 os.remove('content.txt')
 print('done!')
